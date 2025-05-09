6f2c0775995d1db9f5eaa7a400ea56a02f0f07b2
Inochi Amaoto <inochiama@gmail.com>
2025-05-09 10:02:34 +0800

    common: allow use address for read/write
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- common/include/common.h
+++ common/include/common.h
@@ -7,12 +7,12 @@
 #include "mapping.h"
 #include "io.h"
 
-#define readb(c)	({ uint8_t  __v; __io_br(); __v = __raw_readb(c); __io_ar(); __v; })
-#define readw(c)	({ uint16_t __v; __io_br(); __v = __raw_readw(c); __io_ar(); __v; })
-#define readl(c)	({ uint32_t __v; __io_br(); __v = __raw_readl(c); __io_ar(); __v; })
+#define readb(c)	({ uint8_t  __v; __io_br(); __v = __raw_readb((volatile void *)c); __io_ar(); __v; })
+#define readw(c)	({ uint16_t __v; __io_br(); __v = __raw_readw((volatile void *)c); __io_ar(); __v; })
+#define readl(c)	({ uint32_t __v; __io_br(); __v = __raw_readl((volatile void *)c); __io_ar(); __v; })
 
-#define writeb(v,c)	({ __io_bw(); __raw_writeb((v),(c)); __io_aw(); })
-#define writew(v,c)	({ __io_bw(); __raw_writew((v),(c)); __io_aw(); })
-#define writel(v,c)	({ __io_bw(); __raw_writel((v),(c)); __io_aw(); })
+#define writeb(v,c)	({ __io_bw(); __raw_writeb((v),(volatile void *)(c)); __io_aw(); })
+#define writew(v,c)	({ __io_bw(); __raw_writew((v),(volatile void *)(c)); __io_aw(); })
+#define writel(v,c)	({ __io_bw(); __raw_writel((v),(volatile void *)(c)); __io_aw(); })
 
 #endif /* _COMMON_H */
